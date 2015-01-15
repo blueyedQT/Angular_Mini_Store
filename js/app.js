1d18d7173ed0866159eeb3314ef3423acaffc927
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-15 14:54:47 -0800

    Added Orders controller and made fully functional

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ miniStore.config(function($routeProvider){
 	.when('/orders', {templateUrl: 'partials/orders.html'})
 	.when('/customers', {templateUrl: 'partials/customers.html'});
 });
+var customers =[
+	{name: 'Katrina', created_date: 'April 23 2015'},
+	{name: 'Timothy', created_date: 'June 23 2015'},
+	{name: 'Justin', created_date: 'July 23 2015'},
+	{name: 'Zachary', created_date: 'August 23 2015'}
+];
 miniStore.factory('CustomerFactory', function(){
 	var errors = {};
-	var customers =[
-		{name: 'Katrina', created_date: 'April 23 2015'},
-		{name: 'Timothy', created_date: 'June 23 2015'},
-		{name: 'Justin', created_date: 'July 23 2015'},
-		{name: 'Zachary', created_date: 'August 23 2015'}
-	];
 	var factory = {};
 	factory.getCustomers = function(){
 		return customers;
@@ -41,6 +41,40 @@ miniStore.factory('CustomerFactory', function(){
 	};
 	return factory;
 });
+
+miniStore.factory('OrderFactory', function(){
+	var products =[
+		{name: 'Nike Shoes'},
+		{name: 'Black Belts'},
+		{name: 'Ice Cream'},
+		{name: 'Candies'}
+	];
+	var orders = [
+		{customer_name: 'Katrina', product: 'Nike Shoes', quantity: 2, date: 'December 17 2014'},
+		{customer_name: 'Joey', product: 'Ice Creams', quantity: 4, date: 'January 1 2015'}
+	];
+	var factory = {};
+	factory.getCustomers = function(){
+		return customers;
+	};
+	factory.getProducts = function(){
+		return products;
+	};
+	factory.getOrders = function(){
+		return orders;
+	};
+	factory.addOrder = function(info){
+		console.log(info);
+		orders.push({
+			customer_name: info.name,
+			product: info.product,
+			quantity: info.quantity,
+			date: Date.now()
+		});
+	};
+	return factory;
+});
+
 miniStore.controller('Customers', function($scope, CustomerFactory){
 	$scope.customers = CustomerFactory.getCustomers();
 	$scope.addCustomer = function(){
@@ -49,5 +83,14 @@ miniStore.controller('Customers', function($scope, CustomerFactory){
 	$scope.errors = CustomerFactory.getErrors();
 	$scope.removeCustomer = function(name){
 		CustomerFactory.removeCustomer(name);
-	}
+	};
+});
+
+miniStore.controller('Orders', function($scope, OrderFactory){
+	$scope.customers = OrderFactory.getCustomers();
+	$scope.products = OrderFactory.getProducts();
+	$scope.orders = OrderFactory.getOrders();
+	$scope.addOrder = function(){
+		OrderFactory.addOrder($scope.new_order);
+	};
 });
